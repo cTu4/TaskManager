3e892d00246119d941a88dcb83a98f1be12f0ace
cTu4 <chudik9898@mail.ru>
2020-08-14 00:48:04 +0300

    first3

--- db_config.php
+++ db_config.php
@@ -3,6 +3,6 @@
 $host = 'us-cdbr-east-02.cleardb.com'; // адрес сервера
 $database = 'heroku_78c06ec4330d665'; // имя базы данных
 $user = 'bcde434ef6ce7f'; // имя пользователя
-$password = '	f99d83c0'; // пароль
+$password = 'f99d83c0'; // пароль
 
 $mysqli = mysqli_connect($host, $user, $password, $database);
